75ef70c1eab6fbbe7a8f85d2eaee10d161591eb6
Tim <timvdslik@gmail.com>
2016-11-09 14:30:02 +0100

    6th day push

--- cpt/cpt-homes.php
+++ cpt/cpt-homes.php
@@ -338,4 +338,65 @@ function tvds_homes_column_orderby ( $vars ) {
         $vars = array_merge( $vars, array( 'meta_key' => 'place', 'orderby' => 'meta_value' ) );
     }
     return $vars;
-}?>
+}
+
+// Include The Templates
+//----------------------------------------------------------------------------------------------------------------------
+function tvds_homes_include_template_function($template_path){
+    if(get_post_type() == 'homes'){
+        if(is_single()){
+            // checks if the file exists in the theme first,
+            // otherwise serve the file from the plugin
+            if($theme_file = locate_template(array('single-homes.php'))){
+                $template_path = $theme_file;
+            } else {
+                $template_path = plugin_dir_path(__FILE__).'../templates/single-homes.php';
+            }
+        }
+        elseif ( is_archive() ) {
+            if ( $theme_file = locate_template( array ( 'archive-homes.php' ) ) ) {
+                $template_path = $theme_file;
+            } else { $template_path = plugin_dir_path( __FILE__ ) . '../templates/archive-homes.php';
+
+            }
+        }
+    }
+    return $template_path;
+}
+add_filter( 'template_include', 'tvds_homes_include_template_function', 1 );
+
+// Create Taxonomies
+//----------------------------------------------------------------------------------------------------------------------
+function tvds_create_homes_taxonomies(){
+    register_taxonomy(
+        'homes_place',
+        'homes',
+        array(
+            'labels' => array(
+                'name' => __('Plaats', 'tvds'),
+                'add_new_item' => __('Nieuwe plaats', 'tvds'),
+                'new_item_name' => __('Nieuwe plaats type', 'tvds')
+            ),
+            'show_ui' => true,
+            'show_tagcloud' => false,
+            'hierarchical' => true,
+            'rewrite' => array( 'slug' => 'plaats' ),
+        )
+    );
+    register_taxonomy(
+        'homes_type',
+        'homes',
+        array(
+            'labels' => array(
+                'name' => __('Type', 'tvds'),
+                'add_new_item' => __('Nieuw type', 'tvds'),
+                'new_item_name' => __('Nieuw type', 'tvds')
+            ),
+            'show_ui' => true,
+            'show_tagcloud' => false,
+            'hierarchical' => true,
+            'rewrite' => array( 'slug' => 'type' ),
+        )
+    );
+}
+add_action('init', 'tvds_create_homes_taxonomies', 0);
--- vc_elements/vc_elements.php
+++ vc_elements/vc_elements.php
@@ -6,12 +6,12 @@
  * Time: 7:36 PM
  */
 
-//	Spacing.
+//	Runs when all plugins loaded.
 //-----------------------------------------------------------------------------------------------------------------------
-
 add_action( 'plugins_loaded', 'tvds_booking_vc_elements' );
-
 function tvds_booking_vc_elements(){
+    //	Homes Display.
+    //------------------------------------------
     vc_map(array(
         "name"                    	=> __('Huizen', 'tvds'),
         "base"                    	=> 'tvds_homes_display',
@@ -51,13 +51,55 @@ function tvds_booking_vc_elements(){
             ),
         ),
     ));
+
+    //	Homes Search.
+    //------------------------------------------
+    vc_map(array(
+        "name"                    	=> __('Huizen zoeken', 'tvds'),
+        "base"                    	=> 'tvds_homes_search',
+        "description"				=> __('Huizen zoeken', 'tvds'),
+        "icon"                    	=> "",
+        "show_settings_on_create" 	=> true,
+        "category"                	=> __('TVDS Huizen', 'tvds'),
+        "controls"                	=> "full",
+        "save_always" 			  	=> true,
+        "params"                  	=> array(
+            array(
+                "type" 				=> "dropdown",
+                "class" 			=> "",
+                "heading" 			=> __("Style Type", "tvds"),
+                "param_name" 		=> "columns",
+                "value" 			=> array(
+                    __("1 kolom", "tvds") 	    => "col-md-12",
+                    __("2 kolommen", "tvds")    => "col-md-6",
+                    __("3 kolommen", "tvds") 	=> "col-md-4",
+                    __("4 kolommen", "tvds") 	=> "col-md-3",
+                ),
+            ),
+            array(
+                "type" 				=> "textfield",
+                "class" 			=> "",
+                "heading" 			=> __("Aantal posts", "tvds"),
+                "param_name" 		=> "max_posts",
+                "value" 			=> "",
+            ),
+            array(
+                "type" 				=> "textfield",
+                "class" 			=> "",
+                "heading" 			=> __("Extra Class", "tvds"),
+                "param_name" 		=> "el_class",
+                "value" 			=> "",
+                "description" 		=> __( 'If you wish to style particular content element differently, then use this field to add a class name and then refer to it in your css file.', 'themeawesome' )
+            ),
+        ),
+    ));
 }
 
-//	Spacing Shortcode.
+//	Homes Display Shortcode.
 //------------------------------------------------------------
-function tvds_spacing_test_shortcode($atts, $content = null){
+function tvds_homes_display_shortcode($atts, $content = null){
     // Get all the atts from the element
-    $atts = vc_map_get_attributes( 'tvds_homes_search', $atts );
+    $atts = vc_map_get_attributes( 'tvds_homes_display', $atts );
     extract( $atts );
 
     $args = array(
@@ -117,4 +159,75 @@ function tvds_spacing_test_shortcode($atts, $content = null){
     // Return the output
     return $output;
 }
-add_shortcode('tvds_homes_display', 'tvds_spacing_test_shortcode');
+add_shortcode('tvds_homes_display', 'tvds_homes_display_shortcode');
+
+//	Homes Search Shortcode.
+//------------------------------------------------------------
+function tvds_homes_search_shortcode($atts, $content = null){
+    // Get all the atts from the element
+    $atts = vc_map_get_attributes( 'tvds_homes_search', $atts );
+    extract( $atts );
+
+
+    $output = '<div class="tvds_homes_search_form">';
+        $output .= '<div class="tvds_homes_search_form_head">';
+            $output .= '<h3>Huizen zoeken</h3>';
+        $output .= '</div>';
+
+        $output .= '<div class="tvds_homes_search_form_body">';
+
+            $output .= '<form method="get" action="'.home_url().'/homes/">';
+                // Keyword
+                $output .= '<label class="tvds_homes_search_form_keyword" style="float: left; margin-right: 2.427%; width: 31.650%;">';
+                    $output .= '<span>'.__('Zoekwoord', 'tvds').'</span><br/>';
+                    $output .= '<input type="text" name="keyword" style="width: 100%;"/>';
+                $output .= '</label>';
+
+                // Place Filter
+                $output .= '<label class="tvds_homes_search_form_place" style="float: left; margin-right: 2.427%; width: 31.650%;">';
+                    $output .= '<span>'.__('Plaats', 'tvds').'</span><br/>';
+                    $output .= '<input type="text" name="place" style="width: 100%;"/>';
+                $output .= '</label>';
+
+                // Type Filter
+                $output .= '<label class="tvds_homes_search_form_type" style="float: left; width: 31.650%;">';
+                    $output .= '<span>'.__('Type', 'tvds').'</span></br>';
+                    $output .= '<select name="type" style="width: 100%;">';
+                        $output .= '<option value="">Alle Types</option>';
+
+                        // Get All Type Taxonomies
+                        $terms = get_terms('homes_type', array('hide_empty' => false));
+
+                        foreach($terms as $term){
+                            if(isset($type) == $term->slug){
+                                $output .= '<option selected value="'.$term->slug.'">'.$term->name.'</option>';
+                            }
+                            else {
+                                $output .= '<option value="'.$term->slug.'">'.$term->name.'</option>';
+                            }
+                        }
+                    $output .= '</select>';
+                $output .= '</label>';
+
+                // Persons Filter
+                $output .= '<label>'.__('Aantal Personen', 'tvds').'</label></br>';
+                $output .= '<select name="max_persons">';
+                    $output .= '<option value="">Kies aantal personen</option>';
+                    for($x = 1; $x <= 20; $x++){
+                        $output .= '<option value="'.$x.'">'.$x.'</option>';
+                    }
+                $output .= '</select>';
+
+
+
+                $output .= '<input type="submit" value="submit"/>';
+
+            $output .= '</form>';
+
+        $output .= '</div>';
+    $output .= '</div>';
+
+    // Return the output
+    return $output;
+}
+add_shortcode('tvds_homes_search', 'tvds_homes_search_shortcode');
